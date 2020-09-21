fe6cfd1a6143d9ecd11cd7e0fd517e04d234d76a
vsoch <vsochat@stanford.edu>
2020-09-21 15:01:55 -0600

    running pyflakes
    Signed-off-by: vsoch <vsochat@stanford.edu>

--- yeastphenome/apps/datasets/views.py
+++ yeastphenome/apps/datasets/views.py
@@ -23,7 +23,6 @@ from yeastphenome.apps.datasets.utils import get_gene_metadata
 from yeastphenome.apps.conditions.models import ConditionType
 from yeastphenome.apps.phenotypes.models import Observable
 
-from itertools import chain
 from decimal import Decimal
 from libchebipy import ChebiEntity
 import csv
@@ -237,19 +236,6 @@ class Echo:
     def write(self, value):
         """Write the value by returning it, instead of storing in a buffer."""
         return value
-
-
-def some_streaming_csv_view(request):
-    """A view that streams a large CSV file."""
-    # Generate a sequence of rows. The range is based on the maximum number of
-    # rows that can be handled by a single sheet in most spreadsheet
-    # applications.
-    rows = (["Row {}".format(idx), str(idx)] for idx in range(65536))
-    response = StreamingHttpResponse(
-        (writer.writerow(row) for row in rows), content_type="text/csv"
-    )
-    response["Content-Disposition"] = 'attachment; filename="somefilename.csv"'
-    return response
 
 
 @ratelimit(key="ip", rate=rl_rate, block=rl_block)
