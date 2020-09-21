633a7e3f2b232601d3aa1ee241376f962b4657a2
Vanessasaurus <vsochat@stanford.edu>
2020-09-21 15:47:26 -0600

    updating with fixes for gene view, etc. (#25)
    * updating with fixes for gene view, etc.
    * running pyflakes
    
    Signed-off-by: vsoch <vsochat@stanford.edu>

--- yeastphenome/apps/datasets/utils.py
+++ yeastphenome/apps/datasets/utils.py
@@ -6,6 +6,11 @@ SGD_BASE_URL = "https://www.yeastgenome.org/webservice"
 def get_gene_metadata(locus_id):
     try:
         url = "%s/locus/%s" % (SGD_BASE_URL, locus_id)
-        return requests.get(url=url).json()
+        response = requests.get(url=url).json()
+        # Filter down aliases to only include aliases
+        response["aliases"] = [
+            x for x in response.get("aliases", []) if x["category"] == "Alias"
+        ]
+        return response
     except:
         return {}
