813e1a4bfbb2b79dc77c25d47816e18df5e52e39
Vanessasaurus <vsochat@stanford.edu>
2020-09-28 16:40:18 -0600

    More changes to data search to only use GET params (#28)
    * more changes to data search to only use GET params, dataset detail page
    * remove unnecessary console logs
    * filter the results instead of filter records
    
    Signed-off-by: vsoch <vsochat@stanford.edu>

--- yeastphenome/apps/datasets/urls.py
+++ yeastphenome/apps/datasets/urls.py
@@ -8,9 +8,7 @@ urlpatterns = [
         "collection/<int:collection_id>/", views.data_explorer, name="collection_detail"
     ),
     path("genes", views.gene_explorer, name="genes"),
-    path(
-        "gene/download/<str:systematic_name>/", views.download_all, name="download_gene"
-    ),
+    path("download/<str:systematic_name>/", views.download_all, name="download_gene"),
     path(
         "gene/download/sims/<str:systematic_name>/",
         views.download_sims,
@@ -30,8 +28,7 @@ urlpatterns = [
     url(r"^conditions/(?P<domain>chebi)/(?P<id>\d+)/", views.data, name="data"),
     url(r"^(?P<domain>phenotypes)/(?P<id>\d+)/", views.data, name="data"),
     url(r"^(?P<pk>\d+)/$", views.DatasetDetailView.as_view(), name="detail"),
-    url(r"^download/all/", views.download_all, name="download_all"),
-    url(r"^download/", views.download, name="download"),
+    url(r"^download/$", views.download, name="download"),
     url(r"^tag/(?P<id>\d+)/", views.tag, name="tag"),
 ]
 
--- yeastphenome/apps/datasets/utils.py
+++ yeastphenome/apps/datasets/utils.py
@@ -1,6 +1,25 @@
+from wsgiref.util import FileWrapper
+from django.http import StreamingHttpResponse
+
 import requests
+import os
 
 SGD_BASE_URL = "https://www.yeastgenome.org/webservice"
+
+
+def send_file(exported_file, chunk_size=8192):
+    """Send file is shared by similarity and dataset downloads, and streams a chunked response to
+    the server, the expected octet stream
+    """
+    response = StreamingHttpResponse(
+        FileWrapper(open(exported_file, "rb"), chunk_size),
+        content_type="application/octet-stream",
+    )
+    response["Content-Length"] = os.path.getsize(exported_file)
+    response["Content-Disposition"] = "attachment; filename=%s" % os.path.basename(
+        exported_file
+    )
+    return response
 
 
 def get_gene_metadata(locus_id):
