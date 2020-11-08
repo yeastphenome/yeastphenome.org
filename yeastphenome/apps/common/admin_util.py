8996ebfc32d117288556e897bc8a94fd339d0ec1
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2020-11-07 19:41:27 -0800

    Ran black formatting

--- yeastphenome/apps/common/admin_util.py
+++ yeastphenome/apps/common/admin_util.py
@@ -98,7 +98,7 @@ class ImprovedModelAdmin(admin.ModelAdmin):
         return super().response_change(request, obj)
 
     def response_add(self, request, obj, post_url_continue=None):
-        if request.GET.get('_popup') == '1':
+        if request.GET.get("_popup") == "1":
             return HttpResponse(
                 '<script type="text/javascript">window.opener.location.reload(); window.close();</script>'
             )
