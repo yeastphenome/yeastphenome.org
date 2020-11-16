6d4eef4fbfad91e6178f7d2f61e2cd66ce9c6575
vsoch <vsochat@stanford.edu>
2020-11-15 21:02:37 -0700

    changing ?query parameter to be for ?tag for conditions/phenotypes
    Signed-off-by: vsoch <vsochat@stanford.edu>

--- yeastphenome/apps/papers/urls.py
+++ yeastphenome/apps/papers/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.paper_explorer, name="all"),
     path("year/<int:year>/", views.paper_explorer, name="all_year"),
-    path("<int:pk>", views.PaperDetailView.as_view(), name="detail"),
+    path("<int:pk>/", views.PaperDetailView.as_view(), name="detail"),
     path("contributors/", views.ContributorsListView.as_view(), name="contributors"),
     # Data
     path(
